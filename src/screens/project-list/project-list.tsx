4ca44ef13251c3e9354a57ec6e18f7fe61213f91
little man <26587045+qqis314860100@users.noreply.github.com>
2022-02-23 11:27:37 +0800

    style: 装emotion，优化登录注册界面

--- src/screens/project-list/project-list.tsx
+++ src/screens/project-list/project-list.tsx
@@ -5,8 +5,6 @@ import { SearchPanel } from "./search-panel";
 import * as qs from "qs";
 import { useHttp } from "utils/http";
 
-const apiUrl = process.env.REACT_APP_API_URL;
-
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
   const [param, setParam] = useState({ name: "", personId: "" });
